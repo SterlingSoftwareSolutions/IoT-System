93b97170aa280e1948d491771a54b6fbe4eddcbc
Thafani24 <114370326+Thafani24@users.noreply.github.com>
2023-08-30 17:14:21 +0530

    switch to plx

--- App.js
+++ App.js
@@ -4,144 +4,21 @@ import Icon from 'react-native-vector-icons/FontAwesome'; // Assuming you're usi
 import SlideButton from 'rn-slide-button';
 import {
   Text,
-  Alert,
   View,
-  FlatList,
-  Platform,
-  StatusBar,
   StyleSheet,
-  SafeAreaView,
-  NativeModules,
-  useColorScheme,
-  TouchableOpacity,
-  NativeEventEmitter,
-  PermissionsAndroid,
   ImageBackground,
   Image,
 } from 'react-native';
-import BleManager from 'react-native-ble-manager';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
-const BleManagerModule = NativeModules.BleManager;
-const BleManagerEmitter = new NativeEventEmitter(BleManagerModule);
+import { BleManager } from 'react-native-ble-plx'
+const _BleManager = new BleManager();
 
 const App = () => {
-  const peripherals = new Map();
-  const [isScanning, setIsScanning] = useState(false);
-  const [connectedDevices, setConnectedDevices] = useState([]);
-  const [discoveredDevices, setDiscoveredDevices] = useState([]);
-
-  const handleGetConnectedDevices = () => {
-    BleManager.getBondedPeripherals([]).then(results => {
-      for (let i = 0; i < results.length; i++) {
-        let peripheral = results[i];
-        peripheral.connected = true;
-        peripherals.set(peripheral.id, peripheral);
-        setConnectedDevices(Array.from(peripherals.values()));
-      }
-    });
-  };
-  useEffect(() => {
-    BleManager.enableBluetooth().then(() => {
-      console.log('Bluetooth is turned on!');
-    });
-    BleManager.start({ showAlert: false }).then(() => {
-      console.log('BleManager initialized');
-      handleGetConnectedDevices();
-    });
-    let stopDiscoverListener = BleManagerEmitter.addListener(
-      'BleManagerDiscoverPeripheral',
-      peripheral => {
-        peripherals.set(peripheral.id, peripheral);
-        setDiscoveredDevices(Array.from(peripherals.values()));
-      },
-    );
-    let stopConnectListener = BleManagerEmitter.addListener(
-      'BleManagerConnectPeripheral',
-      peripheral => {
-        console.log('BleManagerConnectPeripheral:', peripheral);
-      },
-    );
-    let stopScanListener = BleManagerEmitter.addListener(
-      'BleManagerStopScan',
-      () => {
-        setIsScanning(false);
-        console.log('scan stopped');
-      },
-    );
-    if (Platform.OS === 'android' && Platform.Version >= 23) {
-      PermissionsAndroid.check(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      ).then(result => {
-        if (result) {
-          console.log('Permission is OK');
-        } else {
-          PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-          ).then(result => {
-            if (result) {
-              console.log('User accepted');
-            } else {
-              console.log('User refused');
-            }
-          });
-        }
-      });
-    }
-    return () => {
-      stopDiscoverListener.remove();
-      stopConnectListener.remove();
-      stopScanListener.remove();
-    };
-  }, []);
-  const startScan = () => {
-    if (!isScanning) {
-      BleManager.scan([], 5, true)
-        .then(() => {
-          console.log('Scanning...');
-          setIsScanning(true);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }
-  };
-  // pair with device first before connecting to it
-  const connectToPeripheral = peripheral => {
-    BleManager.createBond(peripheral.id)
-      .then(() => {
-        peripheral.connected = true;
-        peripherals.set(peripheral.id, peripheral);
-        setConnectedDevices(Array.from(peripherals.values()));
-        setDiscoveredDevices(Array.from(peripherals.values()));
-        console.log('BLE device paired successfully');
-      })
-      .catch(() => {
-        console.log('failed to bond');
-      });
-  };
-  // disconnect from device
-  const disconnectFromPeripheral = peripheral => {
-    BleManager.removeBond(peripheral.id)
-      .then(() => {
-        peripheral.connected = false;
-        peripherals.set(peripheral.id, peripheral);
-        setConnectedDevices(Array.from(peripherals.values()));
-        setDiscoveredDevices(Array.from(peripherals.values()));
-        Alert.alert(`Disconnected from ${peripheral.name}`);
-      })
-      .catch(() => {
-        console.log('fail to remove the bond');
-      });
-  };
-  const isDarkMode = useColorScheme() === 'dark';
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
   // render list of bluetooth devices
   function openRoof() {
-    startScan();
-    console.log("Opening Roof");
+    BleManager.start({ showAlert: false }).then(() => {
+      // Success code
+      console.log("Module initialized");
+    }); console.log("Opening Roof");
   }
   function closeRoof() {
     console.log("Closing Roof");
